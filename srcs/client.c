f4776b23419f76ef9a6c73217d0d9a25af602d04
Jinho Heo <uj07077@gmail.com>
2023-12-07 20:11:53 +0900

    fix: time measurement

--- srcs/client.c
+++ srcs/client.c
@@ -1,24 +1,36 @@
 #include "mytest.h"
+#include <stdio.h>
 
 void clientOrientedIo() {
+#ifdef TIMES
+  struct timeval stime, etime;
+  struct timeval time_result;
+#endif
 
-	#ifdef TIMES
-		struct timeval stime, etime;
-		long int time_result;
-	#endif
-		/* Client_oriented_io. Measure io time, communication time, and time for the rest.
-			*/
+  /* Server_oriented_io. Measure IO time, communication time, and time for the
+   * rest. */
 
-	#ifdef TIMES
-		gettimeofday(&stime, NULL);
-	#endif
+#ifdef TIMES
+  gettimeofday(&stime, NULL);
+#endif
 
-	coRun();
+  coRun();
 
-	#ifdef TIMES
-		gettimeofday(&etime, NULL);
-		time_result = etime.tv_usec - stime.tv_usec;
-		printf("Client_oriented_io TIMES == %d %d %ld\n", etime.tv_usec, stime.tv_usec, time_result);
-	#endif
+#ifdef TIMES
+  gettimeofday(&etime, NULL);
+  time_result.tv_sec = etime.tv_sec - stime.tv_sec;
+  time_result.tv_usec = etime.tv_usec - stime.tv_usec;
 
+  // Handle case where end microseconds is less than start microseconds
+  if (etime.tv_usec < stime.tv_usec) {
+    time_result.tv_sec--;
+    time_result.tv_usec += 1000000;
+  }
+
+  // Convert total time to milliseconds
+  long int elapsed_time =
+      time_result.tv_sec * 1000 + time_result.tv_usec / 1000;
+
+  printf("Client_oriented_io elapsed time: %ld ms\n", elapsed_time);
+#endif
 }
--- srcs/server.c
+++ srcs/server.c
@@ -1,22 +1,36 @@
 #include "mytest.h"
+#include <stdio.h>
+
 void serverOrientedIo() {
-	#ifdef TIMES
-        struct timeval stime,etime;
-        long int time_result;
-	#endif
-	/* Server_oriented_io. Measure IO time, communication time, and time for the rest.
-        */
+#ifdef TIMES
+  struct timeval stime, etime;
+  struct timeval time_result;
+#endif
 
-	#ifdef TIMES
-        gettimeofday(&stime, NULL);
-	#endif
-           
-        soRun();
+  /* Server_oriented_io. Measure IO time, communication time, and time for the
+   * rest. */
 
-	#ifdef TIMES
-        gettimeofday(&etime, NULL);
-        time_result = etime.tv_usec - stime.tv_usec;
-        printf("Server_oriented_io TIMES == %d %d %ld\n", etime.tv_usec, stime.tv_usec, time_result);
-	#endif
+#ifdef TIMES
+  gettimeofday(&stime, NULL);
+#endif
 
+  soRun();
+
+#ifdef TIMES
+  gettimeofday(&etime, NULL);
+  time_result.tv_sec = etime.tv_sec - stime.tv_sec;
+  time_result.tv_usec = etime.tv_usec - stime.tv_usec;
+
+  // Handle case where end microseconds is less than start microseconds
+  if (etime.tv_usec < stime.tv_usec) {
+    time_result.tv_sec--;
+    time_result.tv_usec += 1000000;
+  }
+
+  // Convert total time to milliseconds
+  long int elapsed_time =
+      time_result.tv_sec * 1000 + time_result.tv_usec / 1000;
+
+  printf("Server_oriented_io elapsed time: %ld ms\n", elapsed_time);
+#endif
 }
